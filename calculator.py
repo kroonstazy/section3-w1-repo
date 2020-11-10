d70697d7e238afb214538c14d5004c62e015902c
kroonstazy <70473564+kroonstazy@users.noreply.github.com>
2020-11-10 10:46:31 +0900

    calc

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+        if y==0 
+            print("Error : Can't Divided by 0")
+            break
+    return x / y
